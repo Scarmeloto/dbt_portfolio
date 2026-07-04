68a560ec8900e6bf618613029063cb997d1f24d5
rfranco <rfranco@rd.com.br>
2026-07-04 16:06:59 -0300

    Inclusao de objeto

---
+++ models/trusted_chimok/tru_customer.sql
@@ -0,0 +1,70 @@
+{{ config(
+    materialized='incremental',
+    unique_key='sk_cliente',
+    on_schema_change='append_new_columns'
+) }}
+
+WITH base_data AS (
+
+    SELECT DISTINCT
+        customer_id,
+        first_name,
+        last_name,
+        CASE 
+            WHEN company IS NULL OR company = '' THEN 'Unknown'
+            ELSE company 
+        END AS company,
+        city,
+        state,
+        email
+
+    FROM {{ ref('stg_customer') }} 
+
+)
+
+SELECT
+
+    -- Surrogate Key (Hash)
+    CONVERT(
+        VARCHAR(64),
+        HASHBYTES(
+            'SHA2_256',
+            CONCAT(
+                customer_id, '|',
+                first_name, '|',
+                last_name, '|',
+                company, '|',
+                city, '|',
+                state, '|',
+                email
+            )
+        ),
+    2) AS sk_cliente,
+
+    customer_id,
+    first_name,
+    last_name,
+    company,
+    city,
+    state,
+    email,
+
+    GETDATE() AS data_de,
+    CAST('99991231' AS DATETIME) AS data_ate,
+    CAST(1 AS SMALLINT) AS fl_ultima_versao,
+
+    GETDATE() AS load_date,
+
+    -- Hash de controle (mudança de registro)
+    CONVERT(
+        VARCHAR(64),
+        HASHBYTES(
+            'SHA2_256',
+            CONCAT(
+                customer_id, '|',
+                GETDATE()
+            )
+        ),
+    2) AS HashKey
+
+FROM base_data
